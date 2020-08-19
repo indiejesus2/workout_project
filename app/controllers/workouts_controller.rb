115b2f1db9e478db1a4a05c9280fa75275375e22
vedransehovic <vedran.sehovic@gmail.com>
2020-08-18 22:57:39 -0400

    finished CRUD

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,7 +1,48 @@
 class WorkoutsController < ApplicationController
 
+    def index
+        @workouts = Workout.all
+    end
+
     def new
         @workout = Workout.new
+    end
+
+    def create
+        @workout = Workout.new(workout_params)
+        @workout.save
+        redirect_to workout_path(@workout)
+    end
+
+    def show
+        set_workout
+    end
+    
+    def edit
+        set_workout
+    end
+
+    def update
+        set_workout
+        @workout.update(workout_params)
+        redirect_to workout_path(@workout)
+    end
+
+    def destroy
+        set_workout
+        @workout.delete
+        redirect_to workouts_path
+    end
+
+
+    private
+
+    def set_workout
+        @workout = Workout.find_by_id(params[:id])
+    end
+
+    def workout_params
+        params.require(:workout).permit(:date, :calories_burned, :description, :workout_type)
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_193545) do
+ActiveRecord::Schema.define(version: 2020_08_19_021500) do
 
   create_table "workouts", force: :cascade do |t|
     t.text "date"
     t.integer "calories_burned"
     t.string "description"
-    t.string "type"
+    t.string "workout_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
