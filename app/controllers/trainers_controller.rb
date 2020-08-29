8232849631362adcce2311c04193d7334dc30a6d
CelesteSMThomas <celestesmthomas@gmail.com>
2020-08-29 12:01:42 -0700

    Finished Trainer Controller

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,2 +1,42 @@
 class TrainersController < ApplicationController
+    def index
+        @trainers = Trainer.all
+    end
+
+    def new
+        @trainer = Trainer.new
+    end
+
+    def create
+        @trainer = Trainer.new(trainer_params)
+        @trainer.save
+        redirect_to trainer_path(@trainer)
+    end
+
+    def show
+    end
+    
+    def edit
+    end
+
+    def update
+        @trainer.update(trainer_params)
+        redirect_to trainer_path(@trainer)
+    end
+
+    def destroy
+        @trainer.destroy
+        redirect_to trainers_path
+    end
+
+
+    private
+
+    def set_trainer
+        @trainer = Trainer.find_by_id(params[:id])
+    end
+
+    def trainer_params
+        params.require(:trainer).permit(:username, :password)
+    end
 end
